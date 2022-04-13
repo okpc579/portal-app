b3a83dac8cf6a31e379e007799c7157858f07072
Ubuntu <ubuntu@ip-10-0-0-195.ap-northeast-2.compute.internal>
2022-04-13 06:02:36 +0000

    submodule support

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # portal-app
+
+### portal-app.zip을 만들고 버전관리를 진행하는 repository 입니다.
+### openjdk 1.8.0, npm, node(9.11.1에서 테스트 진행)이 설치되어있어야 합니다. - utils/util-install.sh 실행 시 설치 가능
+
+### utils - portal-app의 원활한 작업을 위한 shell script 모음집(공식적으로 지원되는 Shell이 아니기에 스크립트 내용은 예고 없이 언제든 변경 될 수 있습니다.)
+#### utils/variables.yml - zip 생성과 submodule 컨트롤을 위한 변수 파일
+#### utils/git-submodule-update.sh - 처음 git clone 시 submodule update shell 파일
+#### utils/util-install.sh - build를 위한 java, node 설치 지원 shell 파일
+#### utils/git-submodule-change.sh - git submodule의 버전 컨트롤을 지원하는 shell 파일
+#### utils/make-portal-app.sh - portal-app.zip 파일 생성을 지원하는 shell 파일
--- utils/git-submodule-change.sh
+++ utils/git-submodule-change.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
+source variables.yml
 
+cd ../portal-api
+git checkout ${PORTAL_API_COMMIT}
+
+cd ../portal-common-api
+git checkout ${PORTAL_COMMON_API_COMMIT}
+
+cd ../portal-gateway
+git checkout ${PORTAL_GATEWAY_COMMIT}
+
+cd ../portal-log-api
+git checkout ${PORTAL_REGISTRATION_COMMIT}
+
+cd ../portal-registration
+git checkout ${PORTAL_REGISTRATION_COMMIT}
+
+cd ../portal-ssh
+git checkout ${PORTAL_SSH_COMMIT}
+
+cd ../portal-storage-api
+git checkout ${PORTAL_STORAGE_API_COMMIT}
+
+cd ../portal-webadmin
+git checkout ${PORTAL_WEB_ADMIN_COMMIT}
+
+cd ../portal-webuser
+git checkout ${PORTAL_WEB_USER_COMMIT}
+
+cd ../utils
